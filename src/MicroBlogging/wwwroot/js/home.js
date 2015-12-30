0056ef00233535fc6ffe02b64e289b22c629c446
Fausto Bencosme <argenijam@gmail.com>
2015-12-30 12:44:25 -0400

    Home web-ui added.

---
+++ src/MicroBlogging/wwwroot/js/home.js
@@ -0,0 +1,11 @@
+﻿(function() {
+    "use strict";
+
+    var home = angular.module("mb.home", ["mb.login", "mb.signup"]);
+
+    home.controller("HomeController", [
+        "$scope", "$state", function($scope, $state) {
+
+        }
+    ]);
+})();
